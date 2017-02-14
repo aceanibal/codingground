6021ac1ce8be8af31b8afe60ab207bfb0ba77128
aceanibal <aceanibal@gmail.com>
2017-02-14 14:12:29 -0500

    Created by Coding Ground Client

---
+++ New Project-20170214/rectangle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int rectangle(int argc, char *argv[])
+{
+    int i;
+    int b;
+    
+    
+    for (b = 0; b < atoi(argv[1]); b++){
+        
+        for (i = 0; i < atoi(argv[1]) ; i++){
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+}
